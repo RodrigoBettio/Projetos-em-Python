2e91cbbe7c40c05d4f51e8362e9a7d34acbc31cc
Rodrigo <rodriguinhobettiojr1@gmail.com>
2024-05-08 22:56:24 -0300

    Modificações no Código

--- projeto4/projeto4.py
+++ projeto4/projeto4.py
@@ -1,30 +1,50 @@
+# Jogo da Forca: Implemente uma versão simples do jogo da forca. 
+#O programa começa com uma palavra oculta (o usuário não vê) e 
+#o usuário tenta adivinhar a palavra, letra por letra. 
+#O usuário tem um número limitado de tentativas para adivinhar toda a palavra.
 import random
 def escolherPalavra():
-    palavras = [ "Python", "Computador", "Mouse"]
-    palavraEscolhida = random.choice(palavras)
-    return palavraEscolhida
+    from lista_palavras import lista_palavras
+    return random.choice(lista_palavras)
 
 def forca():
-    from forca_imagens import imagem1
-    print(imagem1)
-    palavraEscolhida = escolherPalavra()
+    palavra = escolherPalavra()
+    letras_usuario = []
     chances = 6
-
+    from forca_imagens import imagem1
+    print(imagem1)  
+    from forca_imagens import fases
     while True:
-        ganhou = True
-
-        letraUsuário = input("Digite uma letra que você acha que está na palavra: ").strip()
-        if letraUsuário not in palavraEscolhida:
-            chances -= 1
-            
-        for caracter in palavraEscolhida:
-            if caracter == letraUsuário:
-                print(caracter, end=" ")
+        faseCorreta = fases[chances]
+        print("")
+        print(faseCorreta)
+        for letra in palavra:
+            if letra in letras_usuario:
+                print (letra, end=" ")
             else:
                 print("_", end=" ")
         
-        if ganhou == False or chances == 0:
+        print(f"\nEssas letras já foram utilizadas: {letras_usuario}\n")
+        tentativa = input("Tente adivinhar uma letra para a palavra:")
+        letras_usuario.append(tentativa)
+
+        if tentativa not in palavra:
+            chances -= 1
+
+        ganhou = True
+        for letra in palavra:
+            if letra not in letras_usuario:
+                ganhou = False
+
+        if chances == 0 or ganhou:
             break
-        print(palavraEscolhida)
-        print(chances)
+ 
+    if ganhou:
+        print(f"Parabens, você ganhou restando {chances} chances, a palavra era: {palavra}")
+        
+    else:
+        print(f"\nAcabaram suas chances, você perdeu! A palavra era:{palavra}")
+        print(fases[0])
+        
+            
 forca()
